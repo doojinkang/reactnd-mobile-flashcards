2a366519b9146e3755de2a4546df1c40ab9f4f4f
Doojin Kang <doojin.kang@gmail.com>
2017-11-08 16:36:51 +0900

    Design QuizView

--- components/QuizView.js
+++ components/QuizView.js
@@ -9,15 +9,16 @@ class QuizView extends Component {
     const { questions } = this.props.navigation.state.params.deck
     return (
       <View style={styles.container}>
+        <Text style={styles.count}> 1/2 </Text>
         <Text style={styles.title}>{questions[0].question}</Text>
-        <TextButton onPress={() => console.log('Answer')} reverse>
+        <Text onPress={() => console.log('Answer')} style={styles.answer}>
           Answer
+        </Text>
+        <TextButton onPress={() => console.log('Correct')} color='green'>
+          Correct
         </TextButton>
-        <TextButton onPress={() => console.log('add Card')} reverse>
-          Add Card
-        </TextButton>
-        <TextButton onPress={() => console.log('start Quiz')}>
-          Start Quiz
+        <TextButton onPress={() => console.log('Incorrect')} color='red' >
+          Incorrect
         </TextButton>
       </View>
     )
@@ -25,6 +26,11 @@ class QuizView extends Component {
 }
 
 const styles = StyleSheet.create({
+  count: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -33,10 +39,12 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
+    marginBottom: 40,
   },
-  subtitle: {
+  answer: {
     fontSize: 18,
-    marginBottom: 20,
+    color: 'red',
+    marginBottom: 30,
   },
 })
 
--- components/TextButton.js
+++ components/TextButton.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import { Text, TouchableOpacity, StyleSheet } from 'react-native'
 
-export default function TextButton( {children, onPress, reverse }) {
+export default function TextButton( {children, onPress, color, reverse }) {
+  if ( typeof color === 'undefined' )
+    bgColor = 'purple'
+  else
+    bgColor = color
+
   return (
     <TouchableOpacity
-      style={ reverse? styles.buttonReverse : styles.button }
+      style={ reverse
+            ? [styles.button, {backgroundColor: 'white', borderWidth: 2, borderColor: bgColor}]
+            : [styles.button, {backgroundColor: bgColor}] }
       onPress = {onPress}>
-      <Text style={ reverse? styles.btnTextReverse : styles.btnText }>{children}</Text>
+      <Text style={ reverse
+                  ? [styles.btnText, {color: bgColor}]
+                  : [styles.btnText, {color: 'white'}] }>
+        {children}
+      </Text>
     </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: 'purple',
+    // backgroundColor: 'purple',
+    // borderColor: 'white',
+    // borderWidth: 2,
     padding: 10,
     paddingLeft: 30,
     paddingRight: 30,
@@ -24,25 +37,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   btnText: {
-    color: 'white',
-    fontSize: 22,
-    textAlign: 'center',
-  },
-  buttonReverse: {
-    backgroundColor: 'white',
-    borderWidth: 2,
-    borderColor: 'purple',
-    padding: 10,
-    paddingLeft: 30,
-    paddingRight: 30,
-    margin: 10,
-    height: 45,
-    borderRadius: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  btnTextReverse: {
-    color: 'purple',
+    // color: 'white',
     fontSize: 22,
     textAlign: 'center',
   },
