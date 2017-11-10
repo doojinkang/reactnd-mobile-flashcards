3e3c5716a3334834a631ef221bc1a5fe6d704885
Doojin Kang <doojin.kang@gmail.com>
2017-11-10 22:17:49 +0900

    Flip card in QuizView

--- components/QuizView.js
+++ components/QuizView.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import FlipCard from 'react-native-flip-card'
+
 import TextButton from './TextButton'
 
 class QuizView extends Component {
+  state = {
+    flip: false
+  }
 
   render() {
     const navigate = this.props.navigation.navigate
@@ -10,16 +15,48 @@ class QuizView extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.count}> 1/2 </Text>
-        <Text style={styles.title}>{questions[0].question}</Text>
-        <Text onPress={() => console.log('Answer')} style={styles.answer}>
-          Answer
-        </Text>
-        <TextButton onPress={() => console.log('Correct')} color='green'>
-          Correct
-        </TextButton>
-        <TextButton onPress={() => console.log('Incorrect')} color='red' >
-          Incorrect
-        </TextButton>
+
+        <FlipCard
+          style={styles.card}
+          flip={this.state.flip}
+          friction={6}
+          perspective={1000}
+          flipHorizontal={true}
+          flipVertical={false}
+          clickable={false}
+          alignHeight={false}
+          alignWidth={false}
+          onFlipEnd={(isFlipEnd)=>{console.log('isFlipEnd', isFlipEnd)}}
+        >
+
+          <View style={styles.face}>
+            <Text style={styles.title}>{questions[0].question}</Text>
+            <Text
+              onPress={()=>{this.setState({flip: !this.state.flip})}}
+              style={styles.answer}>
+              Answer
+            </Text>
+            <TextButton onPress={() => console.log('Correct')} color='green'>
+              Correct
+            </TextButton>
+            <TextButton onPress={() => console.log('Incorrect')} color='red' >
+              Incorrect
+            </TextButton>
+          </View>
+
+          <View style={styles.back}>
+            <Text style={styles.title}>{questions[0].answer}</Text>
+            <Text
+              onPress={()=>{this.setState({flip: !this.state.flip})}}
+              style={styles.answer}>
+              Back
+            </Text>
+
+
+          </View>
+
+        </FlipCard>
+
       </View>
     )
   }
@@ -37,6 +74,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  card: {
+    width: 400,
+    marginTop: 100,
+    borderWidth: 0,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  face: {
+    width: 300,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  back: {
+    width: 300,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
   title: {
     fontSize: 24,
     marginBottom: 40,
